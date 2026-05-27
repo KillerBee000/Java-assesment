a4a9ca9d68031f9ee97e87708935fcff2568cfc6
KillerBee000 <ashishgusain59@gmail.com>
2026-05-27 09:29:07 +0530

    Update Task2.java

--- Task2.java
+++ Task2.java
@@ -7,24 +7,4 @@ an Iterator or enhanced for-loop, except through the iterator's own
 methods. 
 
 
-2. What code pattern at line 142 most likely triggered this error?
-
-Most likely pattern:
-
-for (Transaction t : transactions) {
-    if (condition) {
-        transactions.remove(t);   // triggers ConcurrentModificationException
-    }
-}
-
-
-
-
-3. Provide the minimal code change (one or two lines) that resolves this safely.
-
-Use Iterator.remove() instead of List.remove():
-
-Iterator<Transaction> itr = transactions.iterator();
-
-if (condition) itr.remove();
 */
